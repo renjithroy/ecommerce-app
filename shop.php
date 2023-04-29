984a5ff3a2ea06cacde7cefefa30603aa7ca5f87
Renjith Roy <renjithroychittayam@gmail.com>
2023-04-29 09:17:01 +0530

    minor changes

--- shop.php
+++ shop.php
@@ -1,114 +1,105 @@
 <?php
 session_start();
-include('server/connection.php');
+include 'server/connection.php';
 
-if(isset($_POST['search'])){
+if (isset($_POST['search'])) {
 //return searched products
 
 //1.Determine Page no
 
-  if(isset($_GET['page_no']) && $_GET['page_no'] != "")
-  {
-    //if user has already entered the page, then page no is the one that they selected
-    $page_no = $_GET['page_no'];
-  }
-  else
-  {
-    //if user just entered the page, then default page no is 1.
-    $page_no = 1;
-  }
+    if (isset($_GET['page_no']) && $_GET['page_no'] != "") {
+        //if user has already entered the page, then page no is the one that they selected
+        $page_no = $_GET['page_no'];
+    } else {
+        //if user just entered the page, then default page no is 1.
+        $page_no = 1;
+    }
 
-  $category = $_POST['category'];
-  $price = $_POST['price'];
+    $category = $_POST['category'];
+    $price = $_POST['price'];
 
 //2.return no.of products
 
-  //count of product that match the category and price
-  $stmt1 = $conn->prepare("SELECT COUNT(*) As total_records FROM products WHERE prod_category=? AND product_price<=? "); //returns total no.of products in database
+    //count of product that match the category and price
+    $stmt1 = $conn->prepare("SELECT COUNT(*) As total_records FROM products WHERE prod_category=? AND product_price<=? "); //returns total no.of products in database
 
-  $stmt1->bind_param("ii",$category,$price);
+    $stmt1->bind_param("ii", $category, $price);
 
-  $stmt1->execute(); //executes the sql query
+    $stmt1->execute(); //executes the sql query
 
-  $stmt1->bind_result($total_records); //stores result into a variable as array (its an array of result)
+    $stmt1->bind_result($total_records); //stores result into a variable as array (its an array of result)
 
-  $stmt1->store_result();
+    $stmt1->store_result();
 
-  $stmt1->fetch();
+    $stmt1->fetch();
 
 //3. set total no.of products per page
 
-  $total_records_per_page =8;
-  
-  $offset = ($page_no-1) * $total_records_per_page;
+    $total_records_per_page = 8;
 
-  $previous_page = $page_no - 1;
-  $next_page = $page_no + 1;
+    $offset = ($page_no - 1) * $total_records_per_page;
 
-  $adjacents = "2";
+    $previous_page = $page_no - 1;
+    $next_page = $page_no + 1;
 
-  $total_no_of_pages = ceil($total_records/$total_records_per_page); 
+    $adjacents = "2";
+
+    $total_no_of_pages = ceil($total_records / $total_records_per_page);
 
 //4. Get all products
 
-  $stmt2 = $conn->prepare("SELECT * FROM products WHERE prod_category=? AND product_price<=? LIMIT $offset,$total_records_per_page");
-  $stmt2->bind_param("ii",$category,$price);
-  $stmt2->execute();
+    $stmt2 = $conn->prepare("SELECT * FROM products WHERE prod_category=? AND product_price<=? LIMIT $offset,$total_records_per_page");
+    $stmt2->bind_param("ii", $category, $price);
+    $stmt2->execute();
 
-  $products = $stmt2->get_result();
+    $products = $stmt2->get_result();
 
-}
-else //return all products
+} else //return all products
 {
 //1.Determine Page no
 
-  if(isset($_GET['page_no']) && $_GET['page_no'] != "")
-  {
-    //if user has already entered the page, then page_ no is the one that they selected
-    $page_no = $_GET['page_no'];
-  }
-  else
-  {
-    //if user just entered the page, then default page no is 1.
-    $page_no = 1;
-  }
+    if (isset($_GET['page_no']) && $_GET['page_no'] != "") {
+        //if user has already entered the page, then page_ no is the one that they selected
+        $page_no = $_GET['page_no'];
+    } else {
+        //if user just entered the page, then default page no is 1.
+        $page_no = 1;
+    }
 
 //2.return no.of products
 
-  $stmt1 = $conn->prepare("SELECT COUNT(*) As total_records FROM products"); //returns total no.of products in database
+    $stmt1 = $conn->prepare("SELECT COUNT(*) As total_records FROM products"); //returns total no.of products in database
 
-  $stmt1->execute(); //executes the sql query
+    $stmt1->execute(); //executes the sql query
 
-  $stmt1->bind_result($total_records); //stores result into a variable as array (its an array of result)
+    $stmt1->bind_result($total_records); //stores result into a variable as array (its an array of result)
 
-  $stmt1->store_result();
+    $stmt1->store_result();
 
-  $stmt1->fetch();
+    $stmt1->fetch();
 
 //3. set total no.of products per page
 
-  $total_records_per_page = 8;
-  
-  $offset = ($page_no-1) * $total_records_per_page;
+    $total_records_per_page = 8;
 
-  $previous_page = $page_no - 1;
-  $next_page = $page_no + 1;
+    $offset = ($page_no - 1) * $total_records_per_page;
 
-  $adjacents = "2";
+    $previous_page = $page_no - 1;
+    $next_page = $page_no + 1;
 
-  $total_no_of_pages = ceil($total_records/$total_records_per_page); //ex- 12/8 = 1.5 which is ceiled so 2 pages of products
+    $adjacents = "2";
+
+    $total_no_of_pages = ceil($total_records / $total_records_per_page); //ex- 12/8 = 1.5 which is ceiled so 2 pages of products
 
 //4. Get all products
 
-  $stmt2 = $conn->prepare("SELECT * FROM products LIMIT $offset,$total_records_per_page");
+    $stmt2 = $conn->prepare("SELECT * FROM products LIMIT $offset,$total_records_per_page");
 
-  $stmt2->execute();
+    $stmt2->execute();
 
-  $products = $stmt2->get_result();
-
+    $products = $stmt2->get_result();
 
 }
-
 
 ?>
 
@@ -134,6 +125,9 @@ else //return all products
             box-sizing: border-box;
             object-fit: cover;
         } */
+  .to-set-min-height{
+    min-height: 100vh;
+  }
 
     .pagination a {
       color: coral;
@@ -163,7 +157,7 @@ else //return all products
     border: none;
   }
 
-        
+
 
   </style>
 </head>
@@ -190,9 +184,9 @@ else //return all products
           </li>
           <li class="nav-item">
             <a href="cart.php"><i class="fas fa-shopping-bag">
-                <?php if(isset($_SESSION['quantity']) && $_SESSION['quantity']!= 0) {?>
+                <?php if (isset($_SESSION['quantity']) && $_SESSION['quantity'] != 0) {?>
                   <sup class="cart-quantity"><?php echo $_SESSION['quantity']; ?></sup>
-                <?php } ?></i></a>
+                <?php }?></i></a>
             <a href="account.php"><i class="fas fa-user"></i></a>
           </li>
         </ul>
@@ -200,7 +194,7 @@ else //return all products
     </div>
   </nav>
 
-
+<div class="to-set-min-height">
   <!--Search-->
   <section id="search" class="my-5 ms-2" style="padding: 8rem 0 !important;">
     <div class="container mt-5 py-4">
@@ -217,7 +211,7 @@ else //return all products
           <hr style="margin: 10px; padding: 0; height: 2px !important;">
           <div class="form-check">
             <input required class="form-check-input" value=1 type="radio" name="category" id="category_one" <?php
-              if(isset($category) && $category==1 ){echo 'checked' ;}?>>
+if (isset($category) && $category == 1) {echo 'checked';}?>>
             <label class="form-check-label" for="category_one">
               Sarees
             </label>
@@ -225,7 +219,7 @@ else //return all products
 
           <div class="form-check">
             <input required class="form-check-input" value=2 type="radio" name="category" id="category_two" <?php
-              if(isset($category) && $category==2 ){echo 'checked' ;}?>>
+if (isset($category) && $category == 2) {echo 'checked';}?>>
             <label class="form-check-label" for="category_two">
               Shirts
             </label>
@@ -233,13 +227,13 @@ else //return all products
 
           <div class="form-check">
             <input required class="form-check-input" value=3 type="radio" name="category" id="category_three" <?php
-              if(isset($category) && $category==3 ){echo 'checked' ;}?>>
+if (isset($category) && $category == 3) {echo 'checked';}?>>
             <label class="form-check-label" for="category_three">
               Skirt
             </label>
           </div>
 
-         
+
 
         </div>
       </div>
@@ -250,7 +244,7 @@ else //return all products
 
           <p style="margin: 0; padding: 0;">Price</p>
           <hr style="margin: 10px; padding: 0; height: 2px !important;">
-          <input type="range" class="form-range w-50" name="price" value="<?php if(isset($price)){echo $price;}else{ echo " 500";} ?>" min="500" max="3000" id="customRange2">
+          <input type="range" class="form-range w-50" name="price" value="<?php if (isset($price)) {echo $price;} else {echo " 500";}?>" min="500" max="3000" id="customRange2">
 
           <div class="w-50">
             <span style="float: left;">500</span>
@@ -258,9 +252,9 @@ else //return all products
           </div>
         </div>
       </div>
-      
-      
-      
+
+
+
       <!-- <output name="ageOutputName" id="ageOutputId">0</output> -->
       <div class="form-group my-3 mx-3">
         <input type="submit" name="search" value="Search" class="btn btn-primary" style="margin-left: 20px; margin-top: 20px;">
@@ -282,9 +276,9 @@ else //return all products
 
     <div class="row mx-auto container">
 
-<?php if(!isset($_GET['selected_category'])) { ?>
+<?php if (!isset($_GET['selected_category'])) {?>
 
-    <?php while($row = $products->fetch_assoc()){ ?>
+    <?php while ($row = $products->fetch_assoc()) {?>
 
       <div onclick="window.location.href='single_product.php?product_id=<?php echo $row['product_id']; ?>'" class="product text-center col-lg-3 col-md-4 col-sm-12">
         <img class="img-fluid mb-3" src="assets/imgs/<?php echo $row['product_image']; ?>" alt="" />
@@ -300,22 +294,22 @@ else //return all products
         <a class="btn buy-btn" href='single_product.php?product_id=<?php echo $row['product_id']; ?>'>Buy Now</a>
       </div>
 
-    <?php } ?>
+    <?php }?>
 
-<?php } else { ?>
+<?php } else {?>
 
 <!-- saree -->
-    <?php if($_GET['selected_category']=="saree") {?>
+    <?php if ($_GET['selected_category'] == "saree") {?>
 
-          <?php  
-          $stmt3 = $conn->prepare("SELECT * FROM products WHERE prod_category=1 LIMIT $offset,$total_records_per_page");
+          <?php
+$stmt3 = $conn->prepare("SELECT * FROM products WHERE prod_category=1 LIMIT $offset,$total_records_per_page");
 
-          $stmt3->execute();
+    $stmt3->execute();
 
-          $saree_products = $stmt3->get_result();
-          ?>
+    $saree_products = $stmt3->get_result();
+    ?>
 
-              <?php while($row = $saree_products->fetch_assoc()){ ?>
+              <?php while ($row = $saree_products->fetch_assoc()) {?>
 
                   <div onclick="window.location.href='single_product.php?product_id=<?php echo $row['product_id']; ?>'" class="product text-center col-lg-3 col-md-4 col-sm-12">
                     <img class="img-fluid mb-3" src="assets/imgs/<?php echo $row['product_image']; ?>" alt="" />
@@ -331,23 +325,23 @@ else //return all products
                     <a class="btn buy-btn" href='single_product.php?product_id=<?php echo $row['product_id']; ?>'>Buy Now</a>
                   </div>
 
-              <?php } ?>
+              <?php }?>
 
-    <?php } ?>
+    <?php }?>
 
 <!-- shirt -->
 
-    <?php if($_GET['selected_category']=="shirt") {?>
+    <?php if ($_GET['selected_category'] == "shirt") {?>
 
-          <?php  
-          $stmt3 = $conn->prepare("SELECT * FROM products WHERE prod_category=2 LIMIT $offset,$total_records_per_page");
+          <?php
+$stmt3 = $conn->prepare("SELECT * FROM products WHERE prod_category=2 LIMIT $offset,$total_records_per_page");
 
-          $stmt3->execute();
+    $stmt3->execute();
 
-          $shirt_products = $stmt3->get_result();
+    $shirt_products = $stmt3->get_result();
 
-          ?>
-              <?php while($row = $shirt_products->fetch_assoc()){ ?>
+    ?>
+              <?php while ($row = $shirt_products->fetch_assoc()) {?>
 
                   <div onclick="window.location.href='single_product.php?product_id=<?php echo $row['product_id']; ?>'" class="product text-center col-lg-3 col-md-4 col-sm-12">
                     <img class="img-fluid mb-3" src="assets/imgs/<?php echo $row['product_image']; ?>" alt="" />
@@ -363,23 +357,23 @@ else //return all products
                     <a class="btn buy-btn" href='single_product.php?product_id=<?php echo $row['product_id']; ?>'>Buy Now</a>
                   </div>
 
-              <?php } ?>
+              <?php }?>
 
-    <?php } ?>
+    <?php }?>
 
 <!-- skirt -->
 
-    <?php if($_GET['selected_category']=="skirt") {?>
+    <?php if ($_GET['selected_category'] == "skirt") {?>
 
-          <?php  
-          $stmt3 = $conn->prepare("SELECT * FROM products WHERE prod_category=3 LIMIT $offset,$total_records_per_page");
+          <?php
+$stmt3 = $conn->prepare("SELECT * FROM products WHERE prod_category=3 LIMIT $offset,$total_records_per_page");
 
-          $stmt3->execute();
+    $stmt3->execute();
 
-          $skirt_products = $stmt3->get_result();
-          ?>
+    $skirt_products = $stmt3->get_result();
+    ?>
 
-              <?php while($row = $skirt_products->fetch_assoc()){ ?>
+              <?php while ($row = $skirt_products->fetch_assoc()) {?>
 
                   <div onclick="window.location.href='single_product.php?product_id=<?php echo $row['product_id']; ?>'" class="product text-center col-lg-3 col-md-4 col-sm-12">
                     <img class="img-fluid mb-3" src="assets/imgs/<?php echo $row['product_image']; ?>" alt="" />
@@ -395,30 +389,30 @@ else //return all products
                     <a class="btn buy-btn" href='single_product.php?product_id=<?php echo $row['product_id']; ?>'>Buy Now</a>
                   </div>
 
-              <?php } ?>
+              <?php }?>
 
-    <?php } ?>
+    <?php }?>
 
-<?php } ?>
+<?php }?>
 
 
 <!-- Pagination -->
 <nav aria-label="Page navigation example">
   <ul class="pagination mt-5">
     <?php ?>
-          <li class="page-item <?php if($page_no <= 1) {echo 'disabled';}?>">
-            <a class="page-link" href="<?php if($page_no <= 1) {echo '#';} else {echo "?page_no=".($page_no-1);} ?>">Previous</a>
+          <li class="page-item <?php if ($page_no <= 1) {echo 'disabled';}?>">
+            <a class="page-link" href="<?php if ($page_no <= 1) {echo '#';} else {echo "?page_no=" . ($page_no - 1);}?>">Previous</a>
           </li>
           <li class="page-item"><a href="?page_no=1" class="page-link">1</a></li>
           <li class="page-item"><a href="?page_no=2" class="page-link">2</a></li>
 
-          <?php if($page_no >= 3) {?>
+          <?php if ($page_no >= 3) {?>
             <li class="page-item"><a href="" class="page-link">...</a></li>
-            <li class="page-item"><a href="<?php echo "?page_no=".$page_no; ?>" class="page-link"><?php echo $page_no; ?></a></li>            
-          <?php } ?>
+            <li class="page-item"><a href="<?php echo "?page_no=" . $page_no; ?>" class="page-link"><?php echo $page_no; ?></a></li>
+          <?php }?>
 
-          <li class="page-item <?php if($page_no >= $total_no_of_pages){echo 'disabled';} ?>">
-          <a href="<?php if($page_no >= $total_no_of_pages) {echo '#';} else{echo "?page_no=".($page_no+1);} ?>" class="page-link">Next</a>
+          <li class="page-item <?php if ($page_no >= $total_no_of_pages) {echo 'disabled';}?>">
+          <a href="<?php if ($page_no >= $total_no_of_pages) {echo '#';} else {echo "?page_no=" . ($page_no + 1);}?>" class="page-link">Next</a>
         </li>
         </ul>
       </nav>
@@ -426,6 +420,6 @@ else //return all products
     </div>
   </section>
 
+</div>
 
-  
-<?php include('layouts/footer.php') ?>
+<?php include 'layouts/footer.php'?>
